ca6de1b4f90e13cc24e00e1f3d9959f6008df76c
nguyenminhcong2@hoaphat.com.vn <nguyenminhcong2@hoaphat.com.vn>
2025-09-09 07:32:55 +0700

    *

--- src/wwwroot/js/delegation.js
+++ src/wwwroot/js/delegation.js
@@ -4,8 +4,12 @@ $(function () {
     toggleItems.forEach(item => {
         item.addEventListener('click', function (event) {
             if (event.target === this) {
+                var opened = $('.can-toggle.open');
+                console.log(opened.removeClass('open'));
+
                 event.stopPropagation();
                 this.classList.toggle('open');
+                $('#txtgroup').val(this.textContent.trim());
             }
         });
     });
